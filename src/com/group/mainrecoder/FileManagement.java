6e1389fe0780d6678aca99cd6e05e211de223fc9
sadlywish <fred0@sina.com>
2014-10-31 01:15:39 +0800

    完成重命名，保存单一缓存文件方法

--- src/com/group/mainrecoder/FileManagement.java
+++ src/com/group/mainrecoder/FileManagement.java
@@ -37,7 +37,12 @@ public class FileManagement {
 	 * @return 录音文件详细内容列表
 	 */
 	public static List<FileDetail> getMusicFileList(){
-		return null;
+		List<String> nameList = getMusicNameList();
+		List<FileDetail> fileList = new ArrayList<FileDetail>();
+		for (int i = 0; i < nameList.size(); i++) {
+			fileList.add(new FileDetail(nameList.get(i)));
+		}
+		return fileList;
 	}	
 	/**
 	 * @return 录音文件名列表
@@ -47,7 +52,7 @@ public class FileManagement {
 		File[] musics = file.listFiles();
 		List<String> nameList = new ArrayList<String>();
 		for (int i = 0; i < musics.length; i++) {
-			if (musics[i].isFile()) {
+			if (musics[i].isFile() && musics[i].getName().endsWith(".amr")) {
 				nameList.add(musics[i].getName());
 			}
 		}
@@ -57,24 +62,48 @@ public class FileManagement {
 	 * 该方法仅能修改录音文件夹内的文件名
 	 * @param oldName 所需重命名文件的文件名
 	 * @param newName 目标文件名
+	 * @return 修改是否成功
 	 */
-	public static void renameMusicFile(String oldName,String newName){
-		
+	public static boolean renameMusicFile(String oldName,String newName){
+		File oldFile=new File(getPlayerDir()+"/"+oldName); 
+        File newFile=new File(getPlayerDir()+"/"+newName); 
+        if (newFile.exists()) {
+			return false;
+		}
+        oldFile.renameTo(newFile);
+		return true;
 	}
 	/**
 	 * 用于结束录音后将缓存文件重命名并存储至播放文件夹
+	 * 当文件重名时将自动添加后缀
 	 * @param fileName 录音文件命名
+	 * @return 最终存储文件名
 	 */
-	public static void saveTempFile(String fileName){
-		
+	public static String saveTempFile(String fileName){
+		File oldFile=new File(getTempPath()); 
+        File newFile=new File(getPlayerDir()+"/"+fileName); 
+        int i  =1;
+        for (i = 1; i >=1; i++) {
+        	if (newFile.exists()) {
+               newFile=new File(getPlayerDir()+"/"+fileName+"("+i+")"); 
+    		}else {
+				break;
+			}
+		}
+        oldFile.renameTo(newFile);
+        if (i>1) {
+			fileName = fileName+"("+(i-1)+")";
+		}
+		return fileName;
 	}
 	/**
 	 * 用于拼接经过暂停的复数录音缓存文件，并将其重命名并转移至播放文件夹
 	 * @param count 缓存计数器
 	 * @param fileName 最终录音文件名
+	 * @return 最终存储文件名
 	 */
-	public static void saveTempFlies(int count, String fileName){
-		
+	public static String saveTempFlies(int count, String fileName){
+		return null;
 	}
 	/**
 	 * 仅可删除录音文件夹内的文件
@@ -88,12 +117,5 @@ public class FileManagement {
 	 */
 	public static void clearTempDir(){
 		
-	}
-	/**
-	 * @param fileName 需分析文件
-	 * @return 文件详情内容实例
-	 */
-	public static FileDetail getFileDetail(String fileName){
-		return null;
 	}
 }
