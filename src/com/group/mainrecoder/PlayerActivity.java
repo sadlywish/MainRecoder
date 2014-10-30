95e652957d4f1447d76ffb85e43385094a3d30f3
Ma Junjie <majunjie348@yeah.net>
2014-10-31 00:19:42 +0800

    3Player

--- src/com/group/mainrecoder/PlayerActivity.java
+++ src/com/group/mainrecoder/PlayerActivity.java
@@ -1,190 +1,128 @@
 package com.group.mainrecoder;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.media.MediaPlayer.OnCompletionListener;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 
 import com.example.mainrecoder.R;
 
 public class PlayerActivity extends ActionBarActivity {
-	
-	//定义按钮
-		private Button DeleteButton = null;
-		private Button LastButton = null;
-		private Button StarButton = null;
-		private Button StopButton = null;
-		private Button PauseButton = null;
-		private Button NextButton = null;
-	//声明Player
-		private MediaPlayer mPlayer = null;
-	//播放列表
-		private List<String> mList = new ArrayList<String>();
-	//当前播放歌曲的索引
-		private int currentListItem = 0;
-	//音乐路径
-		private static final String MUSIC_PATH = new String ("/sdcard/");
-		
+	final Activity activity = this;
+	// 定义播放器状态（真为有文件在播放）
+	private boolean stauts = false;
+	// 定义按钮
+	private Button DeleteButton = null;
+	private Button LastButton = null;
+	private Button StarButton = null;
+	private Button StopButton = null;
+	private Button PauseButton = null;
+	private Button NextButton = null;
+	// 声明Player
+	private MediaPlayer mPlayer = null;
+
+	// 音乐路径
+	private static final String MUSIC_PATH = new String(Environment
+			.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC)
+			.getPath()
+			+ "/");
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		mList.add("test.amr");
 		setContentView(R.layout.activity_player);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		getSupportActionBar().setTitle("录音播放");
-		
-		//更新显示播放列表
+		// 更新显示播放列表
 		//musicList();
-		
-		//构建Player对象，以及Button
+
+		// 构建Player对象，以及Button
 		mPlayer = new MediaPlayer();
-		
-		DeleteButton = (Button)this.findViewById(R.id.DeleteButton);
-		LastButton = (Button)this.findViewById(R.id.LastButton);
-		StarButton = (Button)this.findViewById(R.id.StarButton);
-		StopButton = (Button)this.findViewById(R.id.StopButton);
-		PauseButton = (Button)this.findViewById(R.id.PauseButton);
-		NextButton = (Button)this.findViewById(R.id.NextButton);
-		
-		//DeleteButton按钮(未完成……)
-		DeleteButton.setOnClickListener(new Button.OnClickListener(){
+		SeekBar bar =  (SeekBar)activity.findViewById(R.id.SeekBar);
+		System.out.println("1");
+		bar.setMax(10000);
+		System.out.println("1");
+
+		DeleteButton = (Button) this.findViewById(R.id.DeleteButton);
+		LastButton = (Button) this.findViewById(R.id.LastButton);
+		StarButton = (Button) this.findViewById(R.id.StarButton);
+		StopButton = (Button) this.findViewById(R.id.StopButton);
+		PauseButton = (Button) this.findViewById(R.id.PauseButton);
+		NextButton = (Button) this.findViewById(R.id.NextButton);
+
+		// DeleteButton按钮(未完成……)
+		DeleteButton.setOnClickListener(new Button.OnClickListener() {
 
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 		});
-		
-		//停止按钮
-		StopButton.setOnClickListener(new Button.OnClickListener(){
+
+		// 停止按钮
+		StopButton.setOnClickListener(new Button.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				// 判断是否在播放
-				if(mPlayer.isPlaying()){
-					mPlayer.reset();//重置mPlayer到初始化状态
-				}				
+				PlayerFactory.stop();
 			}
 		});
-		
-		//开始按钮
-		StarButton.setOnClickListener(new Button.OnClickListener(){
+
+		// 开始按钮
+		StarButton.setOnClickListener(new Button.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				playMusic(MUSIC_PATH+mList.get(currentListItem));//录音播放路径的获取……(未完成)
-				
+				StarButton.setText("暂停");
+				PlayerFactory.play();
 			}
-			
+
 		});
-		
-		//暂停按钮
-		PauseButton.setOnClickListener(new Button.OnClickListener(){
+
+		// 暂停按钮
+		PauseButton.setOnClickListener(new Button.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				if(mPlayer.isPlaying()){
-					//如果在播放，就暂停
-					mPlayer.pause();
-				}else{
-					//如果是暂停，就播放
-					mPlayer.start();
-				}
-				
+				PlayerFactory.pause();	
 			}
-			
-		});
-		
-		//Next按钮
-		NextButton.setOnClickListener(new Button.OnClickListener()
-		{
 
-			@Override
+		});
+
+		// Next按钮
+		NextButton.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(View arg0) {
-				nextMusic();				
+				PlayerFactory.nextMusic();
 			}
-			
 		});
-		
-		//Last按钮
-		LastButton.setOnClickListener(new Button.OnClickListener(){
 
-			@Override
+		// Last按钮
+		LastButton.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				LastMusic();
+				PlayerFactory.LastMusic();
 			}
-			
-		});
-		
-	}
 
-	//播放音乐
-	private void playMusic(String path)
-	{
-		try
-		{
-			//重置mPlayer
-			mPlayer.reset();
-			//设置播放文件的路径
-			mPlayer.setDataSource(path);
-			//准备播放
-			mPlayer.prepare();
-			//开始播放
-			mPlayer.start();
-			//播放完毕后设置监听事件
-			mPlayer.setOnCompletionListener(new OnCompletionListener(){
-				
-				@Override
-				public void onCompletion(MediaPlayer arg0) {
-					// 播放完成后重置
-					mPlayer.reset();
-				}
-			});
-		}catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalStateException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	//下一首
-	private void nextMusic(){
-		if(++currentListItem>=mList.size()){
-			currentListItem=0;
-			playMusic(MUSIC_PATH+mList.get(currentListItem));//从第一首歌开始播放
-		}else{
-			playMusic(MUSIC_PATH+mList.get(currentListItem));
-		}
-	}
-	
-	//上一首
-	private void LastMusic(){
-			if(--currentListItem>0){
-				playMusic(MUSIC_PATH+mList.get(currentListItem));
-			}else{
-				//如果当前currentListItem等于-1，则设置currentListItem=0
-				if(currentListItem<0){
-					currentListItem=0;
-				}
-				playMusic(MUSIC_PATH+mList.get(currentListItem));
-			}	
-	}
-			
+		});
+
+	}	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -200,5 +138,6 @@ public class PlayerActivity extends ActionBarActivity {
 		int id = item.getItemId();
 		return super.onOptionsItemSelected(item);
 	}
-	
+
+
 }
--- src/com/group/mainrecoder/PlayerFactory.java
+++ src/com/group/mainrecoder/PlayerFactory.java
@@ -1,22 +1,35 @@
 package com.group.mainrecoder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.media.MediaPlayer;
 
 /**
- * @author LiuYufei
- * 本类调用时需要进行实例化
- * 请切记在用户离开播放界面时释放资源
+ * @author LiuYufei 本类调用时需要进行实例化 请切记在用户离开播放界面时释放资源
  */
 public class PlayerFactory {
 	/**
 	 * 播放器实例，用于执行相关工作
 	 */
-	static private MediaPlayer mediaPlayer=new MediaPlayer();
+	static private MediaPlayer mediaPlayer = new MediaPlayer();
 	/**
-	 * 播放器是否已装载音频文件
-	 * 用于防止部分异常和错误的产生
+	 * 播放器是否已装载音频文件 用于防止部分异常和错误的产生
 	 */
-	static private boolean isprepare = false;
+	static private boolean isPrepare = false;
+
+	// 播放列表
+	static private List<String> mList = new ArrayList<String>();
+	// 当前播放歌曲的索引
+	static int mListItem = 0;
+
+	// 使用文件名定位列表
+	/**
+	 * @param fileName
+	 */
+	public static void selectByName(String fileName) {
+		
+	}
 
 	public static MediaPlayer getMediaPlayer() {
 		return mediaPlayer;
@@ -25,59 +38,103 @@ public class PlayerFactory {
 	public static void setMediaPlayer(MediaPlayer mediaPlayer) {
 		PlayerFactory.mediaPlayer = mediaPlayer;
 	}
-	
+
 	public static boolean isIsprepare() {
-		return isprepare;
+		return isPrepare;
 	}
 
 	public static void setIsprepare(boolean isprepare) {
-		PlayerFactory.isprepare = isprepare;
+		PlayerFactory.isPrepare = isprepare;
 	}
 
 	/**
 	 * 初始化播放器
-	 * @param path 音频文件路径 
+	 * 
+	 * @param path
+	 *            音频文件路径
 	 */
-	private void inti(String path){
-		
+	private static void inti() {
+		// 重置mPlayer
+		mediaPlayer.reset();
+		// 设置播放文件的路径
+		mediaPlayer.setDataSource(path);
+		// 准备播放
+		mediaPlayer.prepare();
+
+		isPrepare = true;
 	}
+
 	/**
 	 * 播放音频，包括继续播放、初次播放
-	 * @param path 音频文件路径
+	 * 
+	 * @param path
+	 *            音频文件路径
 	 */
-	public void play(String path){
-		
+	public static void play() {
+		if (!isPrepare) {
+			// 当播放器内没有加载音频时，加载新音频
+			inti();// 初始化播放器
+		}
+		mediaPlayer.start();
 	}
+
 	/**
-	 * 更换播放音频，用于上一曲下一曲或顺序播放
-	 * 直接调用本方法，无需手动停止和初始化播放器
+	 * 更换播放音频，用于上一曲下一曲或顺序播放 直接调用本方法，无需手动停止和初始化播放器
+	 * 
 	 * @param path
 	 */
-	public void changePlay(String path){
-		
+	public static void changePlay(String path) {
+
 	}
-	public void pause(){
-		
+
+	public static void pause() {
+		mediaPlayer.pause();
 	}
+
+	// 下一首
+	public static void nextMusic() {
+		stop();
+		if (++mListItem >= mList.size()) {
+			mListItem = 0;
+		}
+		play();
+	}
+
+	// 上一首
+	public static void LastMusic() {
+		stop();
+		if (--mListItem < 0) {
+			mListItem = 0;
+		}
+		play();
+	}
+
 	/**
 	 * 终止播放并释放播放器资源
 	 */
-	public void stop(){
-		
+	public static void stop() {
+		// 判断是否在播放
+		if (mediaPlayer.isPlaying()) 
+		{
+			mediaPlayer.reset(); // 重置mediaPlayer到初始化状态
+			isPrepare = false; // 将播放器内容设置为空
+		}
 	}
+
 	/**
-	 * 当播放器准备好时，返回当前播放进度（0-10000）
-	 * 当播放器未准备好时，返回0
+	 * 当播放器准备好时，返回当前播放进度（0-10000） 当播放器未准备好时，返回0
+	 * 
 	 * @return 播放进度值（0-10000）
 	 */
-	public int getPlayRate(){
+	public static int getPlayRate() {
 		return 0;
 	}
+
 	/**
 	 * 彻底释放该类资源，无视所在状态
 	 */
-	public void release(){
-		
+	public static void release() {
+
 	}
-	
+
 }
