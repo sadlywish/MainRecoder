e79dfc8112a710e1f287e602b5802fc87deb9d33
sadlywish <fred0@sina.com>
2014-10-31 15:02:15 +0800

    完成文件详细信息类构造方法

--- src/com/group/mainrecoder/FileDetail.java
+++ src/com/group/mainrecoder/FileDetail.java
@@ -1,10 +1,20 @@
 package com.group.mainrecoder;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+
 public class FileDetail {
 	private String fileName;// 文件名
 	private long time;// 录音时长
 	private long modiTime;// 最后更改时间
 	private long size;// 文件大小
+	private String[] suffix = {"B","KB","MB","GB","TB"};
 
 	/**
 	 * 利用文件名获取对应信息的构造方法
@@ -12,7 +22,28 @@ public class FileDetail {
 	 * @param filename
 	 */
 	public FileDetail(String filename) {
-
+		File file = new File(Uri.encode(FileManagement.getPlayerDir()+filename));
+		this.fileName = filename;
+		this.modiTime = file.lastModified();
+		this.size = file.length();
+		MediaPlayer mediaPlayer = new MediaPlayer();
+		try {
+			mediaPlayer.setDataSource(file.getAbsolutePath());
+			mediaPlayer.prepare();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.time = mediaPlayer.getDuration();
 	}
 
 	public String getFileName() {
@@ -31,7 +62,8 @@ public class FileDetail {
 	 * @return 字符串化的录音时长
 	 */
 	public String getSringTime() {
-		return null;
+		SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
+		return df.format(new Date(time));
 	}
 
 	public void setTime(long time) {
@@ -39,10 +71,11 @@ public class FileDetail {
 	}
 
 	/**
-	 * @return 文本化的创建时间格式（yy-mm-dd-hh：mm）
+	 * @return 文本化的创建时间格式（yyyy/mm/dd-hh：mm）
 	 */
 	public String getModiTimeTime() {
-		return null;
+		SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd-HH:mm");
+		return df.format(new Date(time));
 	}
 
 	public void setModiTime(long modiTime) {
@@ -50,7 +83,17 @@ public class FileDetail {
 	}
 
 	public String getSize() {
-		return null;
+		double output = this.size;
+		int i =0;
+		for (i = 0; i < suffix.length; i++) {
+			if (output>1024f) {
+				output/=1024f;
+			}else {
+				break;
+			}
+		}
+		DecimalFormat df = new DecimalFormat("#.000");
+		return df.format(output)+" "+suffix[i];
 	}
 
 	public void setSize(long size) {
