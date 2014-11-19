f71ea266d8dfa04664f182c2bee509634b82ede3
sadlywish <fred0@sina.com>
2014-11-19 18:47:45 +0800

    刚刚没刷出来的上传

--- src/com/group/mainrecoder/FragmentPreferences.java
+++ src/com/group/mainrecoder/FragmentPreferences.java
@@ -16,14 +16,16 @@
 
 package com.group.mainrecoder;
 
-
-
 import com.example.mainrecoder.R;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 
 /**
@@ -33,25 +35,48 @@ import android.support.v7.app.ActionBarActivity;
 public class FragmentPreferences extends ActionBarActivity {
 
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        // Display the fragment as the main content.
-        getFragmentManager().beginTransaction().replace(android.R.id.content,
-                new PrefsFragment()).commit();
-    }
+		// Display the fragment as the main content.
+		getFragmentManager().beginTransaction()
+				.replace(android.R.id.content, new PrefsFragment()).commit();
+	}
 
+	@SuppressLint("NewApi")
+	public static class PrefsFragment extends PreferenceFragment implements OnPreferenceChangeListener {
+		 ListPreference lp;//创建一个ListPreference对象
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			// Load the preferences from an XML resource
+			addPreferencesFromResource(R.xml.fragmented_preferences);
+			lp = (ListPreference) findPreference("recoderType");
+			   //获取ListPreference中的实体内容
+			   CharSequence[] entries=lp.getEntries();
+			   //获取ListPreference中的实体内容的下标值
+			   int index=lp.findIndexOfValue(PreferenceManager.getDefaultSharedPreferences(getActivity()).getString("recoderType", "amr"));
+			   //把listPreference中的摘要显示为当前ListPreference的实体内容中选择的那个项目
+			   lp.setSummary(entries[index]);
+			lp.setOnPreferenceChangeListener(this);
+		}
 
-    @SuppressLint("NewApi")
-	public static class PrefsFragment extends PreferenceFragment {
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-
-            // Load the preferences from an XML resource
-            addPreferencesFromResource(R.xml.fragmented_preferences);
-        }
-    }
+		@Override
+		public boolean onPreferenceChange(Preference preference, Object newValue) {
+			 if(preference instanceof ListPreference){
+				   //把preference这个Preference强制转化为ListPreference类型
+				   ListPreference listPreference=(ListPreference)preference;
+				   //获取ListPreference中的实体内容
+				   CharSequence[] entries=listPreference.getEntries();
+				   //获取ListPreference中的实体内容的下标值
+				   int index=listPreference.findIndexOfValue((String)newValue);
+				   //把listPreference中的摘要显示为当前ListPreference的实体内容中选择的那个项目
+				   listPreference.setSummary(entries[index]);
+				  }
+				  return true;
+		}
+	}
+	
+	
 
 }
