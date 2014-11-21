88506534c0cb9a8832c82c25bfda79f1324ff806
kangzhen <758429735@qq.com>
2014-11-21 17:13:20 +0800

    创建Test类

---
+++ src/com/group/mainrecoder/PlayerFactoryTest.java
@@ -0,0 +1,19 @@
+package com.group.mainrecoder;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlayerFactoryTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetFileName() {
+		fail("Not yet implemented");
+	}
+
+}
